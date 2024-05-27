3e499efb935b44c879b62f034b126155a53b7bed
Dilraj Gill <=>
2024-05-27 07:13:18 -0700

    Updated for pre-0.5 testing

---
+++ Src/loggers/downlink_logs.h
@@ -0,0 +1,14 @@
+//
+// Created by dilgill on 5/24/24.
+//
+
+#ifndef DOWNLINK_LOGS_H
+#define DOWNLINK_LOGS_H
+
+#include "experiment_logger.h"
+
+void LOGGER_downlink_exp_logs(struct LocalExpLogs * local_exp_logs);
+
+void LOGGER_downlink_event_logs();
+
+#endif //DOWNLINK_LOGS_H
